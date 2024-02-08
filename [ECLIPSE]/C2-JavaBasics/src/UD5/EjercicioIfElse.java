fe7f67c3b9e3fe639b43a6cde095ea74ce8d3f37
Diego Mejías <mejias.diego19@gmail.com>
2024-02-08 09:14:35 +0100

    Ejercicio IfElse

--- [ECLIPSE]/C2-JavaBasics/src/UD5/EjercicioIfElse.java
+++ [ECLIPSE]/C2-JavaBasics/src/UD5/EjercicioIfElse.java
@@ -4,22 +4,16 @@ public class EjercicioIfElse {
 
 	public static void main(String[] args) {
 
-	int hora= 0;
-		
-		if (hora>=18 && hora<= 23) {
-		System.out.println("Buenas noches");}
-		 else if(hora== 0 || (hora >= 12 && hora <18)) {
-		System.out.println("Buenas tardes");}
-		 else if((hora>=0 && hora < 12)) {
-		System.out.println("Buenos días");
-		 }
-		
+		int hora = 20;
 
-		
-		
-		
-		
-		
+		if (hora >=6 && hora <=12) {
+			System.out.println("Buenos dias");
+		} else if (hora > 12 && hora < 20) {
+			System.out.println("Buenas tardes");
+		} else {
+			System.out.println("Buenas noches");
+		}
+
 	}
 
 }
