5398a59ab8d376adabd3ba24738e4c7da20e6dbb
Diego Mejías <mejias.diego19@gmail.com>
2024-02-19 17:54:01 +0100

    19/02/2024

--- [ECLIPSE]/C2-JavaBasics/src/UD5/UD5_01.java
+++ [ECLIPSE]/C2-JavaBasics/src/UD5/UD5_01.java
@@ -6,27 +6,23 @@ public class UD5_01 {
 
 		int x = 10;
 		int y = 20;
+		
+		boolean resultado1;
+		boolean resultado2;
+		boolean resultado3;
 
-		
-		
-		
-		
-//		boolean resultado1;
-//		boolean resultado2;
-//		boolean resultado3;
-//
-//		resultado1 = y > x;
-//		System.out.println("¿" + x + " es mayor a " + y + "? " + resultado1);
-//
-//		resultado2 = x == y;
-//		System.out.println("¿" + x + " es igual a " + y + "? " + resultado2);
-//
-//		x += 10;
-//		resultado1 = y >= x;
-//		System.out.println("¿" + x + " es mayor o igual que " + y + "? " + resultado1);
-//
-//		resultado2 = x == y;
-//		System.out.println("¿" + x + " es igual a " + y + "? " + resultado2);
+		resultado1 = y > x;
+		System.out.println("¿" + x + " es mayor a " + y + "? " + resultado1);
+
+		resultado2 = x == y;
+		System.out.println("¿" + x + " es igual a " + y + "? " + resultado2);
+
+		x += 10;
+		resultado1 = y >= x;
+		System.out.println("¿" + x + " es mayor o igual que " + y + "? " + resultado1);
+
+		resultado2 = x == y;
+		System.out.println("¿" + x + " es igual a " + y + "? " + resultado2);
 
 	}
 
