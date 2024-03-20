dd2c56a15b23790d93e313ecfd8710482954307a
Diego Mejías <mejias.diego19@gmail.com>
2024-03-20 19:14:57 +0100

    Ejercicio extra 2

---
+++ [ECLIPSE]/C2-JavaBasics/src/UD8/Extras/CuentaBancaria.java
@@ -0,0 +1,51 @@
+package UD8.Extras;
+
+import java.util.Scanner;
+
+public class CuentaBancaria {
+	public String numeroCuenta = "123456789";
+	public double saldo = 0;
+
+	public CuentaBancaria(String numeroCuenta, double saldo) {
+		this.numeroCuenta = "123456789";
+		this.saldo=hacerIngreso(this.saldo);
+		this.saldo=sacarDinero(this.saldo);
+
+	}
+
+	public String getNumeroCuenta() {
+		return numeroCuenta;
+	}
+
+	public void setNumeroCuenta(String numeroCuenta) {
+		this.numeroCuenta = numeroCuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public static double hacerIngreso(double saldo) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Indique el importe a ingresar");
+		double ingreso = 0;
+		ingreso = scanner.nextDouble();
+		saldo+=ingreso;
+		return saldo;
+	}
+		public static double sacarDinero(double saldo) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Indique el importe a sacar");
+			double sacada = 0;
+			sacada = scanner.nextDouble();
+			saldo-=sacada;
+			return saldo;
+	}
+	public static void main(String[] args) {
+		CuentaBancaria cuentaBancaria1 = new CuentaBancaria("123456789", 0);
+		System.out.println(cuentaBancaria1.getSaldo());
+}}
