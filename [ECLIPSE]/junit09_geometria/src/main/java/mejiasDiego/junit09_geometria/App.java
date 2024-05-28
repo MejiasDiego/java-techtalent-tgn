7cf14d8b3ee23cc68c2bda3cfa69004b1ce52cfe
Diego Mejías <mejias.diego19@gmail.com>
2024-05-28 19:01:52 +0200

    Exercise

---
+++ [ECLIPSE]/junit09_geometria/src/main/java/mejiasDiego/junit09_geometria/App.java
@@ -0,0 +1,13 @@
+package mejiasDiego.junit09_geometria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
