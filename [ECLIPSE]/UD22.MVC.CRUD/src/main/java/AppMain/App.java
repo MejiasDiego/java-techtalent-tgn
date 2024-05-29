1ddcaf90765cb2c93378ab85ebf350bd549ae911
Diego Mejías <mejias.diego19@gmail.com>
2024-05-29 20:38:17 +0200

    Ejercicios

---
+++ [ECLIPSE]/UD22.MVC.CRUD/src/main/java/AppMain/App.java
@@ -0,0 +1,15 @@
+package AppMain;
+
+import javax.swing.JLabel;
+
+import views.ventanaIniciar;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ventanaIniciar ventana = new ventanaIniciar();
+
+		
+    }
+}
---
+++ [ECLIPSE]/webServer/src/main/java/mejiasDiego/webServer/App.java
@@ -0,0 +1,13 @@
+package mejiasDiego.webServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
